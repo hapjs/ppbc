342ff8efd1b9975d584365e214d7143c3f127146
hapjs <mailhap@qq.com>
2016-11-12 01:24:36 +0800

    qrcode

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,69 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
+'use strict';
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity,
+  NavigatorIOS,
 } from 'react-native';
 
-export default class ppbc extends Component {
-  render() {
+var QRCodeScreen = require('./qrcode');
+
+var ppbc = React.createClass({
+  render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Index',
+          backButtonTitle: 'Back',
+          component: Index,
+        }}
+      />
     );
   }
-}
+});
 
-const styles = StyleSheet.create({
+var Index = React.createClass({
+
+  render: function() {
+    return (
+      <View style={styles.contentContainer}>
+        <TouchableOpacity onPress={this._onPressQRCode}>
+          <Text>Read QRCode</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  },
+
+  _onPressQRCode: function() {
+    this.props.navigator.push({
+      component: QRCodeScreen,
+      title: 'QRCode',
+      passProps: {
+        onSucess: this._onSucess,
+      },
+    });
+  },
+
+  _onSucess: function(result) {
+    console.log(result);
+  },
+
+});
+
+var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
 
 AppRegistry.registerComponent('ppbc', () => ppbc);
